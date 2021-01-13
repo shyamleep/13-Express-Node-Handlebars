addb2b97d06fa43e270d96c8ddece31a71663a78
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-13 18:33:13 -0500

    connection done

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("connection error");
+        return;
+    }
+    else console.log("connected as id " + PORT)
+});
+
+module.exports = connection; 
