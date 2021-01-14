f963c059421742185fa6957fabf254487468455d
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-13 23:22:28 -0500

    orm?

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ const connection = require("./connection");
 function printQuestionMarks(num) {
     var array = [];
 
-    for (var i=0; i<num; i++) {
+    for (var i = 0; i < num; i++) {
         array.push("?");
     }
 
@@ -21,5 +21,48 @@ function objToSql(obj) {
 };
 
 const orm = {
-    getAll: function( )
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        // console.log(queryString);
+        connection.query(queryString, function (err, res) {
+            if (err) { throw err }
+            cb(res);
+        })
+    },
+
+    insertOne: function (table, columns, values, cb) {
+        console.log(vals)
+        // INSERT INTO <TABLE NAME> (COLUMNS) VALUES (?)
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += columns.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(values.length);
+        queryString += ") ";
+        // console.log(queryString);
+
+        connection.query(queryString, values, function (err, res) {
+            if (err) { throw err };
+            cb(res);
+        })
+
+    },
+
+    updateOne: function (table, columnValues, condition, cb) { 
+        // UPDATE <TABLE NAME> SET <COLUMNVALUES> WHERE CONDITION (is true)
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(columnValues);
+        queryString += " WHERE ";
+        queryString += condition;
+        // console.log(queryString);
+
+        connection.query(queryString, function(err, res) {
+            if (err) {throw err};
+            cb (res);
+        });
+    },
 }
+
+module.exports = orm;
