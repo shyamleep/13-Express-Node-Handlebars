aac70b9e94e56f30d3f8ad9025219baa80c642f5
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-13 19:36:41 -0500

    starting orm

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+const connection = require("./connection");
+
+function printQuestionMarks(num) {
+    var array = [];
+
+    for (var i=0; i<num; i++) {
+        array.push("?");
+    }
+
+    return array.toString();
+};
+
+function objToSql(obj) {
+    var array = []
+}
+
