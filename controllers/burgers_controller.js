d7f3f43f2dfbd93d5edfc4904b5ad72ea568fd0b
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-14 00:31:59 -0500

    server?

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,4 +4,30 @@ var router = express.Router();
 
 // routes
 
+// Index/Home
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+    burger.selectAll(function(burgerData) {
+        res.render("index", { burger_data: burgerData })
+    });
+});
+
+// adding a burger
+router.post("/burgers/add", function(req, res) {
+    burger.insertOne(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/:id", function(req, res) {
+    burger.updateOne(req.params.id, function(result) {
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
 module.exports = router; 
--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-express = require("express");
+const express = require("express");
+const PORT = process.env.PORT || 8000
+const app = express();
+const routes = require("./controllers/burgers_controller");
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(expres.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs( { defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("listening on port " + PORT);
+
+});
+
+
+
+
+
