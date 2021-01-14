49d4e30477f24f6476d8f9e7f2577396f268516f
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-13 20:01:51 -0500

    seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- write 'INSERT' queries to populate the burgers table with at least three entries
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Portobello Mushroom Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sloppy Tempeh Burger", FALSE);
